0b84b0c09fccdf482ac4a68a1863ed6da03f134c
khuongsatou <yoshi240499@gmail.com>
2019-12-02 06:57:49 +0700

    version 2.0 paging 2

---
+++ PhanTrang/WebApplication1/WebForm1.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:TextBox ID="txt_name" runat="server" ValidationGroup="add"></asp:TextBox>
+        <asp:Image ID="Image1" runat="server" Height="118px" Visible="False" Width="121px" />
+        <asp:FileUpload ID="ful_img" runat="server" />
+        <asp:DropDownList ID="ddl_stt" runat="server">
+            <asp:ListItem Value="1">Active</asp:ListItem>
+            <asp:ListItem Value="0">Disabled</asp:ListItem>
+        </asp:DropDownList>
+        <asp:Button ID="btn_submit" runat="server" Text="Add" OnClick="btn_submit_Click" ValidationGroup="add" />
+        <br />
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txt_name" ErrorMessage="RequiredFieldValidator"></asp:RequiredFieldValidator>
+        <br />
+        <div>
+            <asp:TextBox ID="txtKey" runat="server" ValidationGroup="search"></asp:TextBox>
+            <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" ValidationGroup="search" />
+        </div>
+        <table>
+            <asp:Repeater ID="Repeater1" runat="server">
+                <ItemTemplate>
+                    <tr>
+
+                        <td style='background-color:<%# (Convert.ToInt32(Eval("ID").ToString())%2==0?"red":"blue")%>;'><%#Eval("ID")%> 
+                            </td>
+                        <td><%#Eval("name")%></td>
+                        <td>
+                            <img src="img/<%#Eval("img")%>" style="width:100px;height:100px;" alt="Alternate Text" /></td>
+                        <td><%# (Eval("stt").ToString()=="1"?"Active":"Disabled")%></td>
+                        <td><a href="?id=<%#Eval("id")%>">Edit</a></td>
+                    </tr>
+                </ItemTemplate>
+            </asp:Repeater>
+            
+        </table>
+        <div>
+            <asp:Repeater ID="Repeater2" runat="server">
+                <ItemTemplate>
+                    <a href="?page=<%#Eval("index") %><% if (Request["key"] != null) Response.Write("&key=" + Request["key"]); %><% if (Request["id"] != null) Response.Write("&id=" + Request["id"]); %>" style="background-color:<%# (Eval("active").ToString()=="1"?"yellow":"white")%>"><%#Eval("index") %></a>
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+    </form>
+
+</body>
+</html>
---
+++ PhanTrang/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=.\sqlexpress;Initial Catalog=demo;Integrated Security=True");
+            string sQuery = "";
+            if(Request["id"]!=null)
+            {
+                sQuery = "select * from sp where id=" + Request["id"];
+                SqlDataAdapter lst = new SqlDataAdapter(sQuery, con);
+                DataTable sp = new DataTable();
+                lst.Fill(sp);
+                txt_name.Text = sp.Rows[0]["name"].ToString();
+                Image1.Visible = true;
+                Image1.ImageUrl = "~/img/" + sp.Rows[0]["img"].ToString();
+                ddl_stt.SelectedValue = sp.Rows[0]["stt"].ToString();
+                btn_submit.Text = "Update";
+            }
+            sQuery = "Select * from sanpham";
+            if (Request["key"] != null)
+            {
+                sQuery = sQuery + " where name like '%" + Request["key"].ToString() + "%'";
+            }
+            SqlDataAdapter da = new SqlDataAdapter(sQuery, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            
+            int so_item_1trang = 3;
+            int so_trang = dt.Rows.Count / so_item_1trang + (dt.Rows.Count % so_item_1trang==0?0:1);
+            int page = Request["page"] == null?1:Convert.ToInt32(Request["page"]);
+            int from = (page-1)*3;
+            int to = page*3-1;
+            for (int i = dt.Rows.Count - 1; i >= 0;i-- )
+            {
+                if(i<from || i>to)
+                {
+                    dt.Rows.RemoveAt(i);
+                }
+            }
+            Repeater1.DataSource = dt;
+            Repeater1.DataBind();
+            DataTable dtPage = new DataTable();
+            dtPage.Columns.Add("index");
+            dtPage.Columns.Add("active");
+            for(int i=1;i<=so_trang;i++)
+            {
+                DataRow dr = dtPage.NewRow();
+                dr["index"] = i;
+               
+                if ((Request["page"] == null && i == 1) || (Request["page"] != null && Convert.ToInt32(Request["page"]) == i))
+                    dr["active"]=1;
+                else
+                    dr["active"] = 0;
+                dtPage.Rows.Add(dr);
+            }
+            Repeater2.DataSource = dtPage;
+            Repeater2.DataBind();
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("WebForm1.aspx?key=" + txtKey.Text);
+        }
+
+        protected void btn_submit_Click(object sender, EventArgs e)
+        {
+            if(btn_submit.Text=="Add")
+            {
+                //xu li them
+            }
+            else
+            {
+                //xu li update
+            }
+        }
+    }
+}
